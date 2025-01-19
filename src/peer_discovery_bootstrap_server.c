f95f8b6d2d2cf51aebe6fce6b40a945c45ac37c9
leonard.kosta <kostaleonard@gmail.com>
2025-01-19 09:27:44 -0500

    Split peer discovery thread into multiple functions for future testing
    Signed-off-by: leonard.kosta <kostaleonard@gmail.com>

--- src/peer_discovery_bootstrap_server.c
+++ src/peer_discovery_bootstrap_server.c
@@ -24,66 +24,17 @@
 
 #define LISTEN_BACKLOG 5
 
-int main(int argc, char **argv) {
+// TODO make helper functions and put them in peer_discovery.h for testing
+
+// TODO note in docstring that listen_fd is already bound and listening
+return_code_t accept_peer_discovery_requests(
+    int listen_fd,
+    linked_list_t *peer_list,
+    bool print_progress
+) {
     return_code_t return_code = SUCCESS;
-    if (2 != argc) {
-        fprintf(stderr, "Usage: %s [port]\n", argv[0]);
-        return_code = FAILURE_INVALID_COMMAND_LINE_ARGS;
-        goto end;
-    }
-    linked_list_t *peer_list = NULL;
-    return_code = linked_list_create(&peer_list, free, compare_peer_info_t);
-    if (SUCCESS != return_code) {
-        goto end;
-    }
-    int port = strtol(argv[1], NULL, 10);
-    #ifdef _WIN32
-        WSADATA wsaData;
-        if (0 != WSAStartup(MAKEWORD(2, 2), &wsaData)) {
-            return_code = FAILURE_NETWORK_FUNCTION;
-            goto end;
-        }
-    #endif
-    struct sockaddr_in6 server_addr = {0};
-    struct sockaddr_in6 client_addr = {0};
-    int listen_fd = socket(AF_INET6, SOCK_STREAM, 0);
-    if (listen_fd < 0) {
-        return_code = FAILURE_NETWORK_FUNCTION;
-        goto end;
-    }
-    int optval = 1;
-    if (0 != setsockopt(
-        listen_fd,
-        SOL_SOCKET,
-        SO_REUSEADDR,
-        (const void *)&optval,
-        sizeof(optval))) {
-        return_code = FAILURE_NETWORK_FUNCTION;
-        goto end;
-    }
-    int ipv6_v6only = 1;
-    if (0 != setsockopt(
-        listen_fd,
-        IPPROTO_IPV6,
-        IPV6_V6ONLY,
-        (const void *)&ipv6_v6only,
-        sizeof(ipv6_v6only))) {
-        return_code = FAILURE_NETWORK_FUNCTION;
-        goto end;
-    }
-    server_addr.sin6_family = AF_INET6;
-    server_addr.sin6_addr = in6addr_any;
-    server_addr.sin6_port = htons((uint16_t)port);
-    if (bind(
-        listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        return_code = FAILURE_NETWORK_FUNCTION;
-        goto end;
-    }
-    if (listen(listen_fd, LISTEN_BACKLOG) < 0) {
-        return_code = FAILURE_NETWORK_FUNCTION;
-        goto end;
-    }
     while (true) {
+        struct sockaddr_in6 client_addr = {0};
         socklen_t client_len = sizeof(client_addr);
         int conn_fd = accept(
             listen_fd, (struct sockaddr *)&client_addr, &client_len);
@@ -95,32 +46,36 @@ int main(int argc, char **argv) {
             return_code = FAILURE_BUFFER_TOO_SMALL;
             goto end;
         }
-        char client_hostname[NI_MAXHOST] = {0};
-        if (0 != getnameinfo(
-            (struct sockaddr *)&client_addr,
-            client_len,
-            client_hostname,
-            NI_MAXHOST,
-            NULL,
-            0,
-            0)) {
-            return_code = FAILURE_NETWORK_FUNCTION;
-            goto end;
+        if (print_progress) {
+            char client_hostname[NI_MAXHOST] = {0};
+            if (0 != getnameinfo(
+                (struct sockaddr *)&client_addr,
+                client_len,
+                client_hostname,
+                NI_MAXHOST,
+                NULL,
+                0,
+                0)) {
+                return_code = FAILURE_NETWORK_FUNCTION;
+                goto end;
+            }
+            char client_addr_str[INET6_ADDRSTRLEN] = {0};
+            if (NULL == inet_ntop(
+                AF_INET6,
+                &client_addr.sin6_addr,
+                client_addr_str,
+                INET6_ADDRSTRLEN)) {
+                return_code = FAILURE_NETWORK_FUNCTION;
+                goto end;
+            }
+            printf(
+                "Server established connection with %s (%s)\n",
+                client_hostname,
+                client_addr_str);
         }
-        char client_addr_str[INET6_ADDRSTRLEN] = {0};
-        if (NULL == inet_ntop(
-            AF_INET6,
-            &client_addr.sin6_addr,
-            client_addr_str,
-            INET6_ADDRSTRLEN)) {
-            return_code = FAILURE_NETWORK_FUNCTION;
-            goto end;
-        }
-        printf(
-            "Server established connection with %s (%s)\n",
-            client_hostname,
-            client_addr_str);
+        // TODO this is the core of what we need to test--the actual network communication
         char recv_buf[BUFSIZ] = {0};
+        // TODO send and recv calls should be send_all_with_alloc/recv_all_with_alloc or whatever we come up with
         int bytes_received = recv(
             conn_fd, recv_buf, sizeof(command_header_t), 0);
         if (bytes_received < 0) {
@@ -227,12 +182,84 @@ int main(int argc, char **argv) {
             close(conn_fd);
         # endif
     }
+end:
+    return return_code;
+}
+
+// TODO put this function in a pthread wrapper to allow for graceful shutdown, then unit test
+return_code_t run_peer_discovery_bootstrap_server(uint16_t port) {
+    return_code_t return_code = SUCCESS;
+    linked_list_t *peer_list = NULL;
+    return_code = linked_list_create(&peer_list, free, compare_peer_info_t);
+    if (SUCCESS != return_code) {
+        goto end;
+    }
+    #ifdef _WIN32
+        WSADATA wsaData;
+        if (0 != WSAStartup(MAKEWORD(2, 2), &wsaData)) {
+            return_code = FAILURE_NETWORK_FUNCTION;
+            goto end;
+        }
+    #endif
+    int listen_fd = socket(AF_INET6, SOCK_STREAM, 0);
+    if (listen_fd < 0) {
+        return_code = FAILURE_NETWORK_FUNCTION;
+        goto end;
+    }
+    int optval = 1;
+    if (0 != setsockopt(
+        listen_fd,
+        SOL_SOCKET,
+        SO_REUSEADDR,
+        (const void *)&optval,
+        sizeof(optval))) {
+        return_code = FAILURE_NETWORK_FUNCTION;
+        goto end;
+    }
+    int ipv6_v6only = 1;
+    if (0 != setsockopt(
+        listen_fd,
+        IPPROTO_IPV6,
+        IPV6_V6ONLY,
+        (const void *)&ipv6_v6only,
+        sizeof(ipv6_v6only))) {
+        return_code = FAILURE_NETWORK_FUNCTION;
+        goto end;
+    }
+    // TODO set timeout on socket
+    struct sockaddr_in6 server_addr = {0};
+    server_addr.sin6_family = AF_INET6;
+    server_addr.sin6_addr = in6addr_any;
+    server_addr.sin6_port = htons(port);
+    if (bind(
+        listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        return_code = FAILURE_NETWORK_FUNCTION;
+        goto end;
+    }
+    if (listen(listen_fd, LISTEN_BACKLOG) < 0) {
+        return_code = FAILURE_NETWORK_FUNCTION;
+        goto end;
+    }
+    return_code = accept_peer_discovery_requests(listen_fd, peer_list, true);
     #ifdef _WIN32
         closesocket(listen_fd);
         WSACleanup();
     # else
         close(listen_fd);
     #endif
+end:
+    return return_code;
+}
+
+int main(int argc, char **argv) {
+    return_code_t return_code = SUCCESS;
+    if (2 != argc) {
+        fprintf(stderr, "Usage: %s [port]\n", argv[0]);
+        return_code = FAILURE_INVALID_COMMAND_LINE_ARGS;
+        goto end;
+    }
+    uint16_t port = strtol(argv[1], NULL, 10);
+    return_code = run_peer_discovery_bootstrap_server(port);
 end:
     return return_code;
 }
