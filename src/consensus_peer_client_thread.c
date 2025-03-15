41ea8e7157eaf4c83532db06db860e19a81395b5
leonard.kosta <kostaleonard@gmail.com>
2025-03-15 08:35:27 -0400

    Added peer list copy in consensus
    Signed-off-by: leonard.kosta <kostaleonard@gmail.com>

--- src/consensus_peer_client_thread.c
+++ src/consensus_peer_client_thread.c
@@ -17,7 +17,42 @@ return_code_t *run_consensus_peer_client(
     // TODO connect to peer
     // TODO send and receive blockchain
     // TODO update longest chain
-    linked_list_t *peer_info_list_copy = args->peer_info_list;
+    linked_list_t *peer_info_list_copy = NULL;
+    return_code = linked_list_create(
+        &peer_info_list_copy, free, compare_peer_info_t);
+    if (SUCCESS != return_code) {
+        goto end;
+    }
+    return_code = pthread_mutex_lock(&args->peer_info_list_mutex);
+    if (SUCCESS != return_code) {
+        linked_list_destroy(peer_info_list_copy);
+        goto end;
+    }
+    for (node_t *node = args->peer_info_list->head;
+        NULL != node;
+        node = node->next) {
+        peer_info_t *peer = (peer_info_t *)node->data;
+        peer_info_t *peer_copy = calloc(1, sizeof(peer_info_t));
+        if (NULL == peer_copy) {
+            linked_list_destroy(peer_info_list_copy);
+            pthread_mutex_unlock(&args->peer_info_list_mutex);
+            goto end;
+        }
+        memcpy(peer_copy, peer, sizeof(peer_info_t));
+        // This will copy the peers in reverse, but order does not matter here.
+        return_code = linked_list_prepend(peer_info_list_copy, peer_copy);
+        if (SUCCESS != return_code) {
+            free(peer_copy);
+            linked_list_destroy(peer_info_list_copy);
+            pthread_mutex_unlock(&args->peer_info_list_mutex);
+            goto end;
+        }
+    }
+    return_code = pthread_mutex_unlock(&args->peer_info_list_mutex);
+    if (SUCCESS != return_code) {
+        linked_list_destroy(peer_info_list_copy);
+        goto end;
+    }
     node_t *node = peer_info_list_copy->head;
     bool should_stop = *args->should_stop;
     while (!should_stop && NULL != node) {
@@ -32,7 +67,8 @@ return_code_t *run_consensus_peer_client(
     if (args->print_progress) {
         printf("Finished exchanging blockchains with peers.\n");
     }
-// TODO end:
+    return_code = linked_list_destroy(peer_info_list_copy);
+end:
     pthread_mutex_lock(&args->exit_ready_mutex);
     *args->exit_ready = true;
     pthread_cond_signal(&args->exit_ready_cond);
